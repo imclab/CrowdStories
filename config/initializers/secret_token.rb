3941141ce3a12a053e549aa82ed1bb38cd669031
Niklas Begley <niklas.begley@gmail.com>
2013-10-03 19:58:05 +0100

    Installed rails and updated Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdstories::Application.config.secret_key_base = '32cc39d2a4cda0f2bf6a81b78d612947a3ff49e5f5d611e445eb6128be48ec8c07af4e290a73cfadd47578ccfc69d701f6e2b27dc30f8e117ffd8760a15b5447'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crowdstories::Application.config.session_store :cookie_store, key: '_crowdstories_session'
